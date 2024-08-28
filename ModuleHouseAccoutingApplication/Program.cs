32454335ad4b60266c5bd584fd08184921ed3664
Oleksii Khokhlov <alekseu87w@gmail.com>
2024-08-28 14:33:34 +0300

    default WebAPI project added

---
+++ ModuleHouseAccoutingApplication/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+await app.RunAsync();
